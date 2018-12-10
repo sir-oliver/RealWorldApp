0c0809a80b386cd3510d4d07d88a70ce84d8aba9
Oliver Busta <oliver.busta@gmail.com>
2018-12-10 10:04:13 +0100

    Initial

---
+++ lib/test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Foo extends StatefulWidget {
+  @override
+  _FooState createState() => _FooState();
+}
+
+class _FooState extends State<Foo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
